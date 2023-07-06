8464c51c3081fc8aa5bd02f22294c44c88b54567
PigCharid <wp1308052418@gmail.com>
2023-07-06 10:44:04 +0800

    请求带有上下文内容

--- controllers/aichat.js
+++ controllers/aichat.js
@@ -1,19 +1,43 @@
 import { Configuration, OpenAIApi } from "openai";
+import MessageModal from "../models/message.js";
 import * as dotenv from "dotenv";
 dotenv.config();
 export const aichat = async (req, res) => {
-  const { prompt } = req.body;
+  const { id, role, prompt } = req.body;
+
+  console.log("id", id);
+  console.log("role", role);
+  console.log("prompt", prompt);
+  // 检查参数信息
   if (prompt === undefined || prompt) {
     res.status(510).json({ message: "咨询信息不能为空" });
   }
   try {
+    await MessageModal.create({
+      id: id,
+      role: role,
+      content: prompt,
+      createdAt: new Date(),
+    });
+    const histMessages = await MessageModal.find({ id });
+
+    console.log("message", histMessages);
+    // 组上下文对象
+    const messages = histMessages.map(({ role, content }) => ({
+      role,
+      content,
+    }));
+    console.log("aaaa");
+
+    console.log("组成的对象", messages);
+
     const configuration = new Configuration({
       apiKey: process.env.OPENAI_API_KEY,
     });
     const openai = new OpenAIApi(configuration);
     const chatCompletion = await openai.createChatCompletion({
       model: "gpt-3.5-turbo",
-      messages: [{ role: "user", content: prompt }],
+      messages: messages,
     });
     console.log("chatCompletion", chatCompletion);
     console.log(chatCompletion.data.choices[0].message);
